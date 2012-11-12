5dd219af2400a9cabb810908d339d1a846918e32
Neil Bowers <neil@bowers.com>
2012-11-12 23:28:21 +0000

    Import of NEILB/Acme-Experiment-NEILB-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-Experiment-NEILB
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/Acme-Experiment-NEILB-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/Experiment/OALDERS.pm
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+package Acme::Experiment::OALDERS;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Acme::Experiment::NEILB - more experiments with PAUSE permissions
+
+=cut
+
+1;
